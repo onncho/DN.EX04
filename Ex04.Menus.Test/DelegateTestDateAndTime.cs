8875f5b45ec3233a44bea6172addb5595399df98
gal <galrettig@gmail.com>
2015-06-08 23:51:47 +0300

    delegates update

--- Ex04.Menus.Test/DelegateTestDateAndTime.cs
+++ Ex04.Menus.Test/DelegateTestDateAndTime.cs
@@ -6,5 +6,32 @@ namespace Ex04.Menus.Test
 {
     class DelegateTestDateAndTime
     {
+        public string getStringOfTime()
+        {
+            string header = "The time is: " + DateTime.Now.ToString("h:mm:ss tt");
+            string endLine = "\n------ Press any key to finish -------";
+            string formatted = header + endLine;
+            return formatted;
+        }
+        public void printTimeFromString()
+        {
+            string timeToPrint = getStringOfTime();
+            Console.WriteLine(timeToPrint);
+            Console.ReadLine();
+        }
+
+        public string getStringOfDate()
+        {
+            string header = "The time is: " + DateTime.Now.ToString("M/d/yyyy");
+            string endLine = "\n------ Press any key to finish -------";
+            string formatted = header + endLine;
+            return formatted;
+        }
+        public void printDateFromString()
+        {
+            string dateToPrint = getStringOfDate();
+            Console.WriteLine(dateToPrint);
+            Console.ReadLine();
+        }
     }
 }
